50ced81409b80d951476c8c71a2ceefd834cfb07
marcosTulli <marcostulli@hotmail.com>
2023-07-14 23:31:52 -0300

    Add: Create log file on error

--- helpers/errorHelpers.js
+++ helpers/errorHelpers.js
@@ -1,7 +1,27 @@
+let logRepo = require('../repos/logRepo');
+
 let errorHelpers = {
   logErrorsToConsole: (err, req, res, next) => {
     console.log(`Log Entry: ${JSON.stringify(errorHelpers.errorBuilder(err))}`);
     console.log('*'.repeat(80));
+    next(err);
+  },
+  logErrorsToFile: (err, req, res, next) => {
+    let errorObject = errorHelpers.errorBuilder(err);
+    errorObject.requestInfo = {
+      hostName: req.hostName,
+      path: req.path,
+      app: req.app,
+    };
+    logRepo.write(
+      errorObject,
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
     next(err);
   },
   clientErrorHandler: (err, req, res) => {
--- index.js
+++ index.js
@@ -176,6 +176,9 @@ app.use('/api/', router);
 // Configure exception logger to console
 app.use(errorHelper.logErrorsToConsole);
 
+// Configure exception logger to file
+app.use(errorHelper.logErrorsToFile);
+
 // Configure client error handler
 app.use(errorHelper.clientErrorHandler);
 
